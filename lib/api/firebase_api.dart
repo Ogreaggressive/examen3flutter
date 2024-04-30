2a47d05d982496595e51365f3dba187d5eff4b64
Ogreaggressive <nicolascari.rz@gmail.com>
2024-04-30 08:00:16 -0400

    adding multidex support

--- lib/api/firebase_api.dart
+++ lib/api/firebase_api.dart
@@ -1,6 +1,11 @@
 
-
 import 'package:firebase_messaging/firebase_messaging.dart';
+
+Future<void> handleBackgroundMessage(RemoteMessage message) async {
+  print('Title: ${message.notification?.title}');
+  print('body: ${message.notification?.body}');
+  print('payload: ${message.data}');
+}
 
 class FirebaseApi{
   final _firebaseMessaging = FirebaseMessaging.instance;
@@ -9,5 +14,6 @@ class FirebaseApi{
     await _firebaseMessaging.requestPermission();
     final fCMToken = await _firebaseMessaging.getToken();
     print('token : $fCMToken');
+    FirebaseMessaging.onBackgroundMessage(handleBackgroundMessage);
   }
 }
